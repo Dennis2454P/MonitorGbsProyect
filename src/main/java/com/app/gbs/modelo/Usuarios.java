1ef1e99617f6c327d9055480a9b2203c166cf065
Reposito01_Ejemplo <74647380@certus.edu.pe>
2023-06-16 12:00:19 -0500

    borre una variable

--- src/main/java/com/app/gbs/modelo/Usuarios.java
+++ src/main/java/com/app/gbs/modelo/Usuarios.java
@@ -11,8 +11,8 @@ public class Usuarios {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    /*Se borro un email*/
     private String Nombre;
-    private String email;
     private String Apellido_paterno;
     private String Apellido_materno;
     private String Usuario;
@@ -31,12 +31,12 @@ public class Usuarios {
     public void setId(Long id) {
         this.id = id;
     }
-	public Usuarios(Long id, String nombre, String email, String apellido_paterno, String apellido_materno,
+	public Usuarios(Long id, String nombre, String apellido_paterno, String apellido_materno,
 			String usuario, int dNI, int telefono, String correo, String contraseña) {
 		
 		this.id = id;
 		this.Nombre = nombre;
-		this.email = email;
+		
 		this.Apellido_paterno = apellido_paterno;
 		this.Apellido_materno = apellido_materno;
 		this.Usuario = usuario;
@@ -47,9 +47,6 @@ public class Usuarios {
 	}
 	public String getNombre() {
 		return Nombre;
-	}
-	public String getEmail() {
-		return email;
 	}
 	public String getApellido_paterno() {
 		return Apellido_paterno;
